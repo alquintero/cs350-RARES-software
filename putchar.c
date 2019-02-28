ec4deb6c6a802b315c9d2984cb57bc907d6c49da
sophia <strump@brynmawr.edu>
2019-02-28 09:43:44 -0500

    Working with strings! But need to organize use of regs with hardware team

--- putchar.c
+++ putchar.c
@@ -8,28 +8,26 @@ void HERA_main() {
   DLABEL(string_output)
     LP_STRING("Hello")
 
-  SET(R2, 0) // Set R2 to first terminal
-
   // putchar_ord with 1 character
-  SET(R1, char_output)
+  SET(R3, char_output)
   CALL(FP_alt, putchar_ord)
 
-/*  // putchar_ord with string...
-  SET(R1, string_output)
-  LOAD(R5, 0, R1) // R5 has the length of the string
-  INC(R1, 1) // R1 has the first character of the string
+  // putchar_ord with string...
+  SET(R3, string_output)
+  LOAD(R5, 0, R3) // R5 has the length of the string
+  INC(R3, 1) // R3 has the first character of the string
   LABEL(get_string)
     CALL(FP_alt, putchar_ord)
-    DEC(R5, 1) // decrement the number of characters left
-    INC(R1, 1) // R1 goes to the next character
-    BNZ(get_string) */
+    INC(R3, 1) // R3 goes to the next character
+    DEC(R5, 1) // decrement the number of characters left -- THIS WILL SET THE FLAG FOR THE BRANCH
+    BNZR(get_string)
 
   HALT()
 
-  LABEL(putchar_ord) // R1 has the character, R2 has which screen
-    LOAD(R1, 0, R1) // R1 has the character
+  // RETURNS THE CHARACTER FOR HARDWARE IN R1
+  LABEL(putchar_ord) // R3 has the character, R2 has which screen
+    LOAD(R1, 0, R3) // load the current character at location R3 into R1
+    SET(R2, 0) // R2 has the screen (proxy: screen 1)
     RETURN(FP_alt, PC_ret)
-
-
 
 }
