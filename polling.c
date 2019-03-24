46fd3df23a93276357c38b76448754c798f6dd50
alquintero <aquintero@haverford.edu>
2019-03-23 23:06:27 -0400

    organized code and comments

--- polling.c
+++ polling.c
@@ -22,58 +22,56 @@ void HERA_main()
 
   LABEL(infinite_loop)  // while(true)
     SET(R7, 0) // always put characters in buffer from location 0
-
     SET(R9, 3)
     LABEL(simulate_os)  // for (a fixed number of times); // simulate OS
       DEC(R9, 1)
       BNZ(simulate_os)
 
-    //CHECKING IF KEYBOARD1
-    OPCODE(9733) //0010 0110 0000 0101 for loading into R5, if keyboard1 is ready
-    SET(R9, 1) //for checking for character
-    CMP(R5, R9) //returns 0 if equal
-    BNZ(check_keyboard2) // if keyboard1 does not have a character, branch
-      //keyboard1 has a character
-      SET(R3, 0) // set which terminal, to pass to getchar_ord
-      CALL(FP_alt, getchar_ord) // Display that character onto TTY0
-      CALL(FP_alt, putchar_ord) // Display that character onto TTY0
-
-    //CHECKING IF KEYBOARD2
-    LABEL(check_keyboard2)
-    OPCODE(9990) //0010 0111 0000 0110 for loading into R6, if keyboard2 is ready
+      //CHECKING IF KEYBOARD1 HAS CHARACTER
+      OPCODE(9733) //0010 0110 0000 0101 for loading into R5, if keyboard1 is ready
       SET(R9, 1) //for checking for character
-      CMP(R6, R9) // returns 0 if equal
-      BNZ(done_checking) // if keyboard2 does not have a character, branch
-        //keyboard2 has a character
-        CALL(FP_alt, getchar_ord) // get the character
-        SET(R9, 10) // for checking newline
-        CMP(R2, R9) // comparing character to new line (ascii)
+      CMP(R5, R9) //returns 0 if equal
+      BNZ(check_keyboard2) // if keyboard1 does not have a character, branch
+        //keyboard1 has a character
+        SET(R3, 0) // set which terminal, to pass to getchar_ord
+        CALL(FP_alt, getchar_ord) // Display that character onto TTY0
+        CALL(FP_alt, putchar_ord) // Display that character onto TTY0
 
-        // IT IS A NEWLINE
-        BNZ(not_newline) // if that character is not a newline, branch
-          //it's a newline
-          // put the newline into memory
-          STORE(R7, 0, R2) // put that character in memory
-          INC(R7, 1) // go to the next place in memory
+      //KEYBOARD1 IS EMPTY, CHECKING IF KEYBOARD2 HAS CHARACTER
+      LABEL(check_keyboard2)
+        OPCODE(9990) //0010 0111 0000 0110 for loading into R6, if keyboard2 is ready
+        SET(R9, 1) //for checking for character
+        CMP(R6, R9) // returns 0 if equal
+        BNZ(done_checking) // if keyboard2 does not have a character, branch
+          //keyboard2 has a character
+          CALL(FP_alt, getchar_ord) // get the character
+          SET(R9, 10) // for checking newline
+          CMP(R2, R9) // comparing character to new line (ascii)
 
-          //loop through characters in memory and output each one
-          SET(R9, 0) // start looking from location 0 in memory
-          LABEL(buffer_loop)
-            // there are characters in the buffer, so put them
-            LOAD(R4, 0, R9) // load the character at location R9 in memory
-            SET(R9, 10) // for checking for newline
-            CMP(R4, R9) // is the character we just loaded a newline?
-            BZ(done_checking) // yes, so we're done
-            // no...
-            SET(R3, 1) // set which terminal to pass to getchar_ord
-            CALL(FP_alt, getchar_ord)
-            CALL(FP_alt, putchar_ord)
-            SET(R8, 0)
-            STORE(R9, 0, R8) // clear that place in memory for later use
-            INC(R9, 1) // go to the next place in memory
-          BR(buffer_loop) // always branch back because didn't encounter newline in memory
+          //CHECKING IF CHARACTER IS A NEWLINE
+          BNZ(not_newline) // if that character is not a newline, branch
+            //it's a newline
+            STORE(R7, 0, R2) // put that character in memory
+            INC(R7, 1) // go to the next place in memory
+            //loop through characters in memory and output each one
+            SET(R9, 0) // start looking from location 0 in memory
+            
+            LABEL(buffer_loop)
+              // there are characters in the buffer, so put them
+              LOAD(R4, 0, R9) // load the character at location R9 in memory
+              SET(R9, 10) // for checking for newline
+              CMP(R4, R9) // is the character we just loaded a newline?
+                BZ(done_checking) // yes, so we're done
+                  // no...
+                  SET(R3, 1) // set which terminal to pass to getchar_ord
+                  CALL(FP_alt, getchar_ord)
+                  CALL(FP_alt, putchar_ord)
+                  SET(R8, 0)
+                  STORE(R9, 0, R8) // clear that place in memory for later use
+                  INC(R9, 1) // go to the next place in memory
+                  BR(buffer_loop) // always branch back because didn't encounter newline in memory
 
-        // IT'S NOT A NEWLINE
+        //CHARACTER IS NOT A NEWLINE
         LABEL(not_newline) // else
           //it's not a newline
           CALL(FP_alt, getchar_ord) // load from buffer
